60d2679f13b4e3f728cbe84a49ec65e1199b4a7f
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-29 09:54:36 +0530

    Added:Print array elements logic

---
+++ PrintArrElements.java
@@ -0,0 +1,15 @@
+
+class PrintArrElements {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		System.out.println("Array elements are:");
+		for(int elements=0;elements<arr.length;elements++) {
+			System.out.print(" " +arr[elements]);
+			
+		}
+	
+
+	}
+
+}
