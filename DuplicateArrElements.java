370b6cbb5882031fdec93c7cada7278ab12543e8
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-29 12:39:05 +0530

    Added: Duplicate array elements logic

---
+++ DuplicateArrElements.java
@@ -0,0 +1,15 @@
+
+class DuplicateArrElements {
+	public static void main(String[] args) {
+		int arr[]=new int[] {1,2,3,2,4,5,5};
+		System.out.println("Duplicate elements are :");
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]==arr[j])
+					System.out.println(arr[j]);
+			}
+		}
+
+	}
+
+}
