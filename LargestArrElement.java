a308061102625fa9484565c8691562e16fab37c8
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-29 10:08:54 +0530

    Added:Largest array element logic

---
+++ LargestArrElement.java
@@ -0,0 +1,14 @@
+
+class LargestArrElement {
+	public static void main(String[] args) {
+		int arr[]=new int[] {23,56,78,34,11};
+		int max=arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>max)
+				max=arr[i];
+		}
+		System.out.println("Largest number in the array :" +max);
+
+	}
+
+}
