23025316a84355b0164b5b382c8a6ea5132353d1
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-29 12:31:32 +0530

    Added: Array elements at odd position logic

---
+++ OddPositionArrElements.java
@@ -0,0 +1,11 @@
+class OddPositionArrElements {
+	public static void main(String[] args) {
+		int arr[]=new int[] {1,2,3,4,5};
+		System.out.println("Array elements at odd position:");
+		for(int i=0;i<arr.length;i=i+2) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
